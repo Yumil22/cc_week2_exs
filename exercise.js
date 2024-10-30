38c81085f1582fc7aa5f74b2efce3d8af5907826
RaulMetriTorres <raul.metri@gmail.com>
2024-10-30 03:19:31 -0600

    semana 2 ejercicios js completado

--- exercise.js
+++ exercise.js
@@ -16,7 +16,24 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const merged = (arrA, arrB) => {
     /* Only make changes below this comment */
-    return 1;
+    let result = [];
+    let i = 0, j = 0;
+
+    while (i < arrA.length && j < arrB.length) {
+        if (arrA[i] < arrB[j]) {
+            result.push(arrA[i]);
+            i++;
+        } else {
+            result.push(arrB[j]);
+            j++;
+        }
+    }
+
+    // Añadir los elementos restantes
+    while (i < arrA.length) result.push(arrA[i++]);
+    while (j < arrB.length) result.push(arrB[j++]);
+
+    return result;
     /* Only make changes below this comment */
 }
 
@@ -32,7 +49,15 @@ const merged = (arrA, arrB) => {
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const substrGen = (str) => {
     /* Only make changes below this comment */
-    
+    let substrings = [];
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = i + 1; j <= str.length; j++) {
+            substrings.push(str.slice(i, j));
+        }
+    }
+
+    return substrings;
     /* Only make changes below this comment */
 }
 
@@ -48,8 +73,13 @@ const substrGen = (str) => {
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const spinal = (str) => {
     /* Only make changes below this comment */
-    
-    
+
+// Reemplaza los espacios y guiones bajos con guiones
+return str
+.split(/\s|_|(?=[A-Z])/)
+.join('-')
+.toLowerCase();
+
     /* Only make changes below this comment */
 }
 
@@ -59,8 +89,21 @@ const spinal = (str) => {
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const bubbleSort = (arr) => {
     /* Only make changes below this comment */
-    
-    return arr
+    let n = arr.length;
+    let swapped;
+
+    do {
+        swapped = false;
+        for (let i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+                swapped = true;
+            }
+        }
+        n--; // Reduce el tamaño del array que se ordena
+    } while (swapped);
+
+    return arr;
     /* Only make changes below this comment */
 }
 
